f593c1422494ee6a8dfa56e909cbd70f0092ea13
MaximZykin <zikinmax95@mail.ru>
2022-10-16 22:10:18 +0700

    add text

--- HelloWord/ViewController.swift
+++ HelloWord/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWordLable: UILabel!
+    
+    @IBOutlet var logButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //по умолчанию текст Hello World скрыт
+        helloWordLable.isHidden = true
+        logButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
-
+    
+    //если текст скрыт, то при нажатии кнопки отображается текст и меняется текст самой кнопки
+    @IBAction func makeButonAction() {
+        if helloWordLable.isHidden {
+            helloWordLable.isHidden = false
+            logButton.setTitle("Hide Text", for: .normal)
+        } else {
+            helloWordLable.isHidden = true
+            logButton.setTitle("Show text", for: .normal)
+        }
+    }
+    
 
 }
 
